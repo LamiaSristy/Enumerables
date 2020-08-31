2ee44f85ff8c04235fc6193f7953427486bf8f9c
lamiasristy <lamiasristy@gmail.com>
2020-09-01 01:04:53 +0200

    Built with method my_all, my_any and my_none.

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -15,7 +15,7 @@ module Enumerable
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
-    i = 0 
+    i = 0
     while i < to_a.length
       yield(to_a[i], i)
       i += 1
@@ -28,16 +28,52 @@ module Enumerable
     return to_enum(:my_select) unless block_given?
 
     new_arr = []
-    i = 0
-    while i < to_a.length
-      if yield to_a[i]
-        new_arr << to_a[i]
-      end
-      i += 1
-    end
+    to_a.my_each { |item| new_arr << item if yield item }
     new_arr
   end
 
+  # 3.my_all?
+  def my_all?(param = nil)
+    flag = true
+    if  param.class == Class
+       to_a.my_each { |item| return false if !(item.class == param || (item.class).superclass == param)  }
+
+    elsif param.class == Regexp
+        to_a.my_each {|item| return false if !(item.match(param))}
+
+    elsif !block_given?
+        to_a.my_each {|item| return false if item.nil? || !item}
+    else
+        to_a.my_each {|item| return false if !yield item }
+    end
+    flag
+  end
+
+  # 3.my_any?
+  def my_any?(param = nil) 
+
+    flag = false
+     
+    if  param.class == Class 
+        to_a.my_each {|item| return true if (item.class == param || (item.class).superclass == param)}
+    
+    elsif param.class == Regexp    
+        to_a.my_each {|item| return true  if (item.match(param))}
+
+    elsif !block_given?
+       
+        to_a.my_each {|item| return true if !(item.nil? || !item)}
+
+    else
+        to_a.my_each {|item| return true if yield item }
+    end       
+    flag
+  end
+
+  # 3.my_none?
+  def my_none?(param = nil) 
+    !my_any?(&Proc.new)
+  end
 
 end
 
--- main.rb
+++ main.rb
@@ -11,3 +11,27 @@ friends.my_each_with_index { |friend, index| puts friend if index.even? }
 puts '3.--------my_select--------'
 puts friends.my_select { |friend| friend != 'Brian' }
 
+puts '4.--------my_all--------'
+# puts %w[ant bear cat].my_all? { |word| word.length >= 3 }
+# puts %w[ant bear cat].my_all? { |word| word.length >= 4 }
+# puts %w[ant bear cat].my_all?(/t/)
+# puts [1, 2i, 3.14].my_all?(Numeric)
+# puts [].my_all? 
+
+puts '5.--------my_any--------'
+# puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+# puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+# puts %w[ant bear cat].my_any?(/d/)                        #=> false
+# puts [nil, true, 99].my_any?(Integer)                     #=> true
+# puts [nil, true, 99].my_any?                              #=> true
+# puts [].any?                                           #=> false
+
+puts '6.--------my_none--------'
+# puts %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+# puts %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+# puts %w{ant bear cat}.my_none?(/d/)                        #=> true
+# puts [1, 3.14, 42].my_none?(Float)                         #=> false
+# puts [].my_none?                                           #=> true
+# puts [nil].my_none?                                        #=> true
+# puts [nil, false].my_none?                                 #=> true
+# puts [nil, false, true].my_none?                           #=> false
