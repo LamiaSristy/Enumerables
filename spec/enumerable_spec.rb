590b0108718e34bf40435889c3372f94d5f88c7e
lamiasristy <lamiasristy@gmail.com>
2020-09-15 17:52:13 +0200

    Built with fixed rubocop errors.

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -13,15 +13,15 @@ RSpec.describe 'my enumerable methods' do
     end
 
     it 'should return each array items' do
-      expect(my_array.my_each { |el| el }).to eq(my_array.each { |el| el })
+      expect(my_array.my_each { | x|  x }).to eq(my_array.each { | x|  x })
     end
 
     it 'should return each item from a given range' do
-      expect(my_range.my_each { |el| el }).to eq(my_range.each { |el| el })
+      expect(my_range.my_each { | x|  x }).to eq(my_range.each { | x|  x })
     end
 
-    it 'should return each item key a given hash-rage' do
-      expect(my_hash.my_each { |key, _value| key }).to eq(my_hash.each { |key, _value| key })
+    it 'should return each item  k a given hash-rage' do
+      expect(my_hash.my_each { | k, _ v|  k }).to eq(my_hash.each { | k, _ v|  k })
     end
   end
 
@@ -30,46 +30,46 @@ RSpec.describe 'my enumerable methods' do
       expect(my_array.my_each_with_index.instance_of?(Enumerator)).to be(true)
     end
 
-    it 'should return every even element' do
-      expect(my_array.my_each_with_index { |el, index| el if index.even? }).to eql(my_array.each_with_index { |el, index| el if index.even? })
+    it 'should return every even  xement' do
+      expect(my_array.my_each_with_index { | x, index|  x if index.even? }).to eql(my_array.each_with_index { | x, index|  x if index.even? })
     end
 
-    it 'should return every even element' do
-      expect(my_array.my_each_with_index { |el, index| el if index % 3 == 0 }).to eql(my_array.each_with_index { |el, index| el if index % 3 == 0 })
+    it 'should return every even  xement' do
+      expect(my_array.my_each_with_index { | x, index|  x if index % 3 == 0 }).to eql(my_array.each_with_index { | x, index|  x if index % 3 == 0 })
     end
 
     it 'should return each item from a given range' do
-      expect(my_range.my_each_with_index { |el| el }).to eq(my_range.each { |el| el })
+      expect(my_range.my_each_with_index { | x|  x }).to eq(my_range.each { | x|  x })
     end
 
-    it 'should return each item key a given hash-rage' do
-      expect(my_hash.my_each_with_index { |key, _value| key }).to eq(my_hash.each_with_index { |key, _value| key })
+    it 'should return each item  k a given hash-rage' do
+      expect(my_hash.my_each_with_index { | k, _ v|  k }).to eq(my_hash.each_with_index { | k, _ v|  k })
     end
   end
 
-  context 'test my_select' do
+  context 'test my_s xect' do
     it 'should return enumerator object if block is not given' do
-      expect(my_array.my_select.instance_of?(Enumerator)).to be(true)
+      expect(my_array.my_s xect.instance_of?(Enumerator)).to be(true)
     end
 
-    it 'should return every element except the selected element' do
-      expect(my_array.my_select { |el| el != 'Brian' }).to eql(my_array.select { |el| el != 'Brian' })
+    it 'should return every  xement except the s xected  xement' do
+      expect(my_array.my_s xect { | x|  x != 'Brian' }).to eql(my_array.s xect { | x|  x != 'Brian' })
     end
 
-    it 'should return every selected element' do
-      expect(my_array.my_select { |el| el == 'Brian' }).to eql(my_array.select { |el| el == 'Brian' })
+    it 'should return every s xected  xement' do
+      expect(my_array.my_s xect { | x|  x == 'Brian' }).to eql(my_array.s xect { | x|  x == 'Brian' })
     end
 
-    it 'should return element from range' do
-      expect(my_range.my_select { |el| el == 5 }).to eql(my_range.select { |el| el == 5 })
+    it 'should return  xement from range' do
+      expect(my_range.my_s xect { | x|  x == 5 }).to eql(my_range.s xect { | x|  x == 5 })
     end
 
-    it 'should return every element except the selected element' do
-      expect(my_range.my_select { |el| el != 5 }).to eql(my_range.select { |el| el != 5 })
+    it 'should return every  xement except the s xected  xement' do
+      expect(my_range.my_s xect { | x|  x != 5 }).to eql(my_range.s xect { | x|  x != 5 })
     end
 
-    it 'should select the key which is equal to blocks condition' do
-      expect(my_hash.my_select { |key, _value| key == :a }).to eql({ a: 'abs' })
+    it 'should s xect the  k which is equal to blocks condition' do
+      expect(my_hash.my_s xect { | k, _ v|  k == :a }).to eql({ a: 'abs' })
     end
   end
 
@@ -82,28 +82,28 @@ RSpec.describe 'my enumerable methods' do
       expect([1, 2i, 3.14].my_all?(Numeric)).to eq(true)
     end
 
-    it 'should check if regex is consists in every element and return false' do
+    it 'should check if regex is consists in every  xement and return false' do
       expect(%w[ant bear cat].my_all?(/t/)).to eq(false)
     end
 
-    it 'should check if regex is consists in every element and return true' do
+    it 'should check if regex is consists in every  xement and return true' do
       expect(%w[ant bat cat].my_all?(/t/)).to eq(true)
     end
 
-    it 'should check if every element length is equal 3' do
+    it 'should check if every  xement length is equal 3' do
       expect(%w[ant bat cat].my_all? { |word| word.length == 3 }).to eq(true)
     end
 
-    it 'should check if every element length is greater than 3' do
+    it 'should check if every  xement length is greater than 3' do
       expect(my_array.my_all? { |word| word.length > 3 }).to eq(false)
     end
 
-    it 'should check if every element inside range is positive' do
-      expect(my_range.my_all? { |el| el.positive? }).to eq(true)
+    it 'should check if every  xement inside range is positive' do
+      expect(my_range.my_all? { | x|  x.positive? }).to eq(true)
     end
 
-    it 'should check if every element inside range is negative' do
-      expect((-3..-90).my_all? { |el| el.negative? }).to eq(true)
+    it 'should check if every  xement inside range is negative' do
+      expect((-3..-90).my_all? { | x|  x.negative? }).to eq(true)
     end
   end
 
@@ -116,45 +116,45 @@ RSpec.describe 'my enumerable methods' do
       expect([1, 'f', 3.14].my_any?(Numeric)).to eq(true)
     end
 
-    it 'should check if regex is consists in every element and return false' do
+    it 'should check if regex is consists in every  xement and return false' do
       expect(%w[ant bear cat].my_any?(/t/)).to eq(true)
     end
 
-    it 'should check if regex is consists in any element and return false' do
+    it 'should check if regex is consists in any  xement and return false' do
       expect(%w[ant bat cat].my_any?(/l/)).to eq(false)
     end
 
-    it 'should check if any element length is equal 4' do
+    it 'should check if any  xement length is equal 4' do
       expect(%w[lion bat cat].my_any? { |word| word.length == 4 }).to eq(true)
     end
 
-    it 'should check if any element length is greater than 3' do
+    it 'should check if any  xement length is greater than 3' do
       expect(%w[ant bat cat].my_any? { |word| word.length > 3 }).to eq(false)
     end
 
-    it 'should check if any element inside range is positive' do
-      expect((-3..8).my_any? { |el| el.positive? }).to eq(true)
+    it 'should check if any  xement inside range is positive' do
+      expect((-3..8).my_any? { | x|  x.positive? }).to eq(true)
     end
 
-    it 'should check if any element inside range is negative' do
-      expect(my_range.my_any? { |el| el.negative? }).to eq(false)
+    it 'should check if any  xement inside range is negative' do
+      expect(my_range.my_any? { | x|  x.negative? }).to eq(false)
     end
   end
 
   context 'test my_none' do
-    it 'should return true if none of the element has length of 5' do
+    it 'should return true if none of the  xement has length of 5' do
       expect(%w[ant bear cat].my_none? { |word| word.length == 5 }).to be true
     end
 
-    it 'should return false if none of the element has length of greater than 4' do
+    it 'should return false if none of the  xement has length of greater than 4' do
       expect(%w[ant bear cat].my_none? { |word| word.length >= 4 }).to be false
     end
 
-    it 'should return true if none of the element is Regex /d/' do
+    it 'should return true if none of the  xement is Regex /d/' do
       expect(%w[ant bear cat].my_none?(/d/)).to be true
     end
 
-    it 'should return false if none of the element is Floatx /d/' do
+    it 'should return false if none of the  xement is Floatx /d/' do
       expect([1, 3.14, 42].my_none?(Float)).to be false
     end
 
@@ -162,7 +162,7 @@ RSpec.describe 'my enumerable methods' do
       expect([].my_none?).to be true
     end
 
-    it 'should return true if its an array with falsy elements ' do
+    it 'should return true if its an array with falsy  xements ' do
       expect([nil, false].my_none?).to be true
     end
   end
@@ -194,33 +194,33 @@ RSpec.describe 'my enumerable methods' do
       expect(my_order.my_map { |x| x.gsub('medium', 'extra large') }).to eql(['extra large Big Mac', 'extra large fries', 'extra large milkshake'])
     end
 
-    it 'should return the array with squared element of each element' do
+    it 'should return the array with squared  xement of each  xement' do
       expect(my_range.my_map { |i| i * i }).to eq([25, 36, 49, 64, 81, 100])
     end
 
-    it 'should return the array with squared element of each element' do
+    it 'should return the array with squared  xement of each  xement' do
       my_proc = proc { |x| x + 2 }
       expect(my_range.my_map(my_proc)).to eq([7, 8, 9, 10, 11, 12])
     end
   end
 
   context 'test my_inject' do
-    it 'should return the sum of all the elements' do
+    it 'should return the sum of all the  xements' do
       expect(my_range.my_inject { |sum, n| sum + n }).to eq(45)
     end
 
-    it 'should return the multiplication of all the elements startting with the given number' do
+    it 'should return the multiplication of all the  xements startting with the given number' do
       expect((1..5).my_inject(1) { |product, n| product * n }).to eq(120)
     end
 
-    it 'should return one string by concating array elements' do
+    it 'should return one string by concating array  xements' do
       expect(['I ', 'love ', 'ice water'].my_inject { |acc, cur| acc + cur }).to eq('I love ice water')
     end
   end
 
-  context 'test multiply_els' do
-    it 'should return the multiplication of all the elements' do
-      expect(multiply_els([2, 4, 5])).to eq(40)
+  context 'test multiply_ xs' do
+    it 'should return the multiplication of all the  xements' do
+      expect(multiply_ xs([2, 4, 5])).to eq(40)
     end
   end
 
